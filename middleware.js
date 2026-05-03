d7907edb243c7ac2e922b252301dd17acc3abaf8
CoderKP <pkartikey5757@gmail.com>
2026-05-04 03:03:25 +0530

    Add middleware function for subdomain redirection

---
+++ middleware.js
@@ -0,0 +1,18 @@
+export function middleware(req) {
+  const host = req.headers.get("host");
+  const subdomain = host.split(".")[0];
+
+  if (subdomain === "insightengine") {
+    return Response.redirect("https://hackrx-bajaj-api.onrender.com/docs");
+  }
+
+  if (subdomain === "invisidata") {
+    return Response.redirect("https://invisi-data.onrender.com/");
+  }
+
+  if (subdomain === "wonderlust") {
+    return Response.redirect("https://wonderlust-o9ym.onrender.com/");
+  }
+
+  return new Response("Project not found");
+}
